bd8c3a1b4353b5677ab181c5a20e41739114154d
Ju Yejin <linenive@naver.com>
2023-01-08 16:55:25 +0900

    Adafruit_ST7735 라이브러리와 호환되도록 코드 추가

--- Adafruit_SPITFT.h
+++ Adafruit_SPITFT.h
@@ -187,6 +187,15 @@ class Adafruit_SPITFT : public Adafruit_GFX {
     void         startWrite(void);
     // Chip deselect and/or hardware SPI transaction end as needed:
     void         endWrite(void);
+    
+    void sendCommand(uint8_t commandByte, uint8_t *dataBytes,
+                      uint8_t numDataBytes);
+     void sendCommand(uint8_t commandByte, const uint8_t *dataBytes = NULL,
+                      uint8_t numDataBytes = 0);
+     void sendCommand16(uint16_t commandWord, const uint8_t *dataBytes = NULL,
+                        uint8_t numDataBytes = 0);
+     uint8_t readcommand8(uint8_t commandByte, uint8_t index = 0);
+     uint16_t readcommand16(uint16_t addr);
 
     // These functions require a chip-select and/or SPI transaction
     // around them. Higher-level graphics primitives might start a
